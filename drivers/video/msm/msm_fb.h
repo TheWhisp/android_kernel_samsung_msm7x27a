79db3977e572eb0a0b73aab9b6972199ace2886e
Dāvis Mālnieks <daavis.90@gmail.com>
2013-07-21 21:05:19 +0300

    samsung msm7x27a: add bootlogo support

--- drivers/video/msm/msm_fb.h
+++ drivers/video/msm/msm_fb.h
@@ -219,8 +219,10 @@ int msm_fb_check_frame_rate(struct msm_fb_data_type *mfd,
 				struct fb_info *info);
 
 #ifdef CONFIG_FB_MSM_LOGO
-#define INIT_IMAGE_FILE "/initlogo.rle"
-int load_565rle_image(char *filename, bool bf_supported);
+extern int draw_rgb888_black_screen(void);
+#endif
+#if !defined(CONFIG_TARGET_LOCALE_EUR_VODA)
+extern int charging_boot;
 #endif
 
 #endif /* MSM_FB_H */
