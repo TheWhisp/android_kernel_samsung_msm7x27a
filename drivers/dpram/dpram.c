765ab7a6d82519e16a009091aee97a0e2f37637a
Dāvis Mālnieks <daavis.90@gmail.com>
2013-07-23 01:17:37 +0300

    dpram: possible RIL fix

--- drivers/dpram/dpram.c
+++ drivers/dpram/dpram.c
@@ -1096,7 +1096,7 @@ static int dpram_tty_write_room(struct tty_struct *tty)
 	return 0;
 }
 
-static int dpram_tty_ioctl(struct tty_struct *tty, struct file *file,
+static int dpram_tty_ioctl(struct tty_struct *tty,
 		unsigned int cmd, unsigned long arg)
 {
 	unsigned int val;
