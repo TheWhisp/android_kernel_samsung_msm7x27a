74fedee78e7265b5af8999dbbfdaa142695781ed
Dāvis Mālnieks <daavis.90@gmail.com>
2013-07-24 11:34:30 +0300

    jena: add/update/fix regulators

--- arch/arm/mach-msm/board-msm7x27a-regulator.c
+++ arch/arm/mach-msm/board-msm7x27a-regulator.c
@@ -21,7 +21,7 @@
 #define n_ranges VOLTAGE_RANGE(750000, 1525000, 12500)
 
 #define s_ranges (VOLTAGE_RANGE(700000, 1500000, 12500) + \
-			VOLTAGE_RANGE(1500000, 3050000, 25000))
+		VOLTAGE_RANGE(1500000, 3050000, 25000))
 
 #define PCOM_VREG_CONSUMERS(name) \
 	static struct regulator_consumer_supply __pcom_vreg_supply_##name[]
@@ -38,7 +38,7 @@
 		.max_uV = _max_uV, \
 		.valid_modes_mask = REGULATOR_MODE_NORMAL, \
 		.valid_ops_mask = REGULATOR_CHANGE_VOLTAGE | \
-				  REGULATOR_CHANGE_STATUS, \
+		 REGULATOR_CHANGE_STATUS, \
 		.input_uV = _supply_uV, \
 		.apply_uV = _apply_uV, \
 		.boot_on = _boot_on, \
@@ -74,45 +74,53 @@
 PCOM_VREG_CONSUMERS(smps1) = {
 	REGULATOR_SUPPLY("smps1",	NULL),
 	REGULATOR_SUPPLY("msmc1",	NULL),
+	REGULATOR_SUPPLY("vreg_msmc1",	NULL),
 };
 
 PCOM_VREG_CONSUMERS(smps2) = {
 	REGULATOR_SUPPLY("smps2",	NULL),
 	REGULATOR_SUPPLY("msmc2",	NULL),
+	REGULATOR_SUPPLY("vreg_msmc2",	NULL),
 };
 
 PCOM_VREG_CONSUMERS(smps3) = {
 	REGULATOR_SUPPLY("smps3",	NULL),
 	REGULATOR_SUPPLY("msme1",	NULL),
+	REGULATOR_SUPPLY("vreg_pa",		NULL),
 };
 
 PCOM_VREG_CONSUMERS(smps4) = {
 	REGULATOR_SUPPLY("smps4",	NULL),
 	REGULATOR_SUPPLY("rf",		NULL),
+	REGULATOR_SUPPLY("vreg_msme",	NULL),
 };
 
 PCOM_VREG_CONSUMERS(ldo01) = {
 	REGULATOR_SUPPLY("ldo01",	NULL),
 	REGULATOR_SUPPLY("ldo1",	NULL),
 	REGULATOR_SUPPLY("rfrx1",	NULL),
+	REGULATOR_SUPPLY("vreg_vib",		NULL),
 };
 
 PCOM_VREG_CONSUMERS(ldo02) = {
 	REGULATOR_SUPPLY("ldo02",	NULL),
 	REGULATOR_SUPPLY("ldo2",	NULL),
 	REGULATOR_SUPPLY("rfrx2",	NULL),
+	REGULATOR_SUPPLY("vreg_rf",	NULL),
 };
 
 PCOM_VREG_CONSUMERS(ldo03) = {
 	REGULATOR_SUPPLY("ldo03",	NULL),
 	REGULATOR_SUPPLY("ldo3",	NULL),
 	REGULATOR_SUPPLY("mddi",	NULL),
+	REGULATOR_SUPPLY("vreg_msm_dsi",	NULL),
 };
 
 PCOM_VREG_CONSUMERS(ldo04) = {
 	REGULATOR_SUPPLY("ldo04",	NULL),
 	REGULATOR_SUPPLY("ldo4",	NULL),
 	REGULATOR_SUPPLY("pllx",	NULL),
+	REGULATOR_SUPPLY("vreg_msm_pll",	NULL),
 };
 
 PCOM_VREG_CONSUMERS(ldo05) = {
@@ -125,74 +133,88 @@ PCOM_VREG_CONSUMERS(ldo06) = {
 	REGULATOR_SUPPLY("ldo06",	NULL),
 	REGULATOR_SUPPLY("ldo6",	NULL),
 	REGULATOR_SUPPLY("wlan3",	NULL),
+	REGULATOR_SUPPLY("vcamc",		NULL),
 };
 
 PCOM_VREG_CONSUMERS(ldo07) = {
 	REGULATOR_SUPPLY("ldo07",	NULL),
 	REGULATOR_SUPPLY("ldo7",	NULL),
 	REGULATOR_SUPPLY("msma",	NULL),
+	REGULATOR_SUPPLY("vreg_msma",		NULL),
 };
 
 PCOM_VREG_CONSUMERS(ldo08) = {
 	REGULATOR_SUPPLY("ldo08",	NULL),
 	REGULATOR_SUPPLY("ldo8",	NULL),
 	REGULATOR_SUPPLY("tcxo",	NULL),
+	REGULATOR_SUPPLY("vreg_tcxo",	NULL),
 };
 
 PCOM_VREG_CONSUMERS(ldo09) = {
 	REGULATOR_SUPPLY("ldo09",	NULL),
 	REGULATOR_SUPPLY("ldo9",	NULL),
 	REGULATOR_SUPPLY("usb2",	NULL),
+	REGULATOR_SUPPLY("vreg_usb18",	NULL),
 };
 
 PCOM_VREG_CONSUMERS(ldo10) = {
 	REGULATOR_SUPPLY("ldo10",	NULL),
 	REGULATOR_SUPPLY("emmc",	NULL),
+	REGULATOR_SUPPLY("vbt",		NULL),
 };
 
 PCOM_VREG_CONSUMERS(ldo11) = {
 	REGULATOR_SUPPLY("ldo11",	NULL),
 	REGULATOR_SUPPLY("wlan_tcx0",	NULL),
+	REGULATOR_SUPPLY("vreg_adc",		NULL),
 };
 
 PCOM_VREG_CONSUMERS(ldo12) = {
 	REGULATOR_SUPPLY("ldo12",	NULL),
 	REGULATOR_SUPPLY("gp2",		NULL),
+	REGULATOR_SUPPLY("vreg_msmp",		NULL),
 };
 
 PCOM_VREG_CONSUMERS(ldo13) = {
 	REGULATOR_SUPPLY("ldo13",	NULL),
 	REGULATOR_SUPPLY("mmc",		NULL),
+	REGULATOR_SUPPLY("vreg_tflash",	NULL),
 };
 
 PCOM_VREG_CONSUMERS(ldo14) = {
 	REGULATOR_SUPPLY("ldo14",	NULL),
 	REGULATOR_SUPPLY("usb",		NULL),
+	REGULATOR_SUPPLY("vreg_usb30",	NULL),
 };
 
 PCOM_VREG_CONSUMERS(ldo15) = {
 	REGULATOR_SUPPLY("ldo15",	NULL),
 	REGULATOR_SUPPLY("usim2",	NULL),
+	REGULATOR_SUPPLY("vcamio",		NULL),
 };
 
 PCOM_VREG_CONSUMERS(ldo16) = {
 	REGULATOR_SUPPLY("ldo16",	NULL),
 	REGULATOR_SUPPLY("ruim",	NULL),
+	REGULATOR_SUPPLY("vreg_usim",		NULL),
 };
 
 PCOM_VREG_CONSUMERS(ldo17) = {
 	REGULATOR_SUPPLY("ldo17",	NULL),
 	REGULATOR_SUPPLY("bt",		NULL),
+	REGULATOR_SUPPLY("vcama",	NULL),
 };
 
 PCOM_VREG_CONSUMERS(ldo18) = {
 	REGULATOR_SUPPLY("ldo18",	NULL),
 	REGULATOR_SUPPLY("rftx",	NULL),
+	REGULATOR_SUPPLY("vreg_rfrx",	NULL),
 };
 
 PCOM_VREG_CONSUMERS(ldo19) = {
 	REGULATOR_SUPPLY("ldo19",	NULL),
 	REGULATOR_SUPPLY("wlan4",	NULL),
+	REGULATOR_SUPPLY("vlcd",	NULL),
 };
 
 PCOM_VREG_CONSUMERS(ncp)   = {
@@ -209,6 +231,31 @@ static struct proccomm_regulator_info msm7x27a_pcom_vreg_info[] = {
 	 * S = supply voltage (uV)
 	 * T = type of regulator (smps, pldo, nldo)
 	 *            name   id  supp  min uV    max uV  R   P  A  B  V  S  T*/
+#ifdef CONFIG_MACH_JENA
+	PCOM_VREG_SMP(smps1, 3, NULL, 1100000, 1100000, 0, -1, 0, 1, 0, 0, s),
+	PCOM_VREG_SMP(smps2, 4, NULL, 1100000, 1100000, 0, -1, 0, 0, 0, 0, s),
+	PCOM_VREG_SMP(smps3, 2, NULL, 1800000, 1800000, 0, -1, 1, 1, 0, 0, s),
+	PCOM_VREG_SMP(smps4, 24, NULL, 2100000, 2100000, 0, -1, 0, 0, 0, 0, s),
+	PCOM_VREG_LDO(ldo01, 12, NULL, 3000000, 3300000, 0, -1, 0, 0, 0, 0, p),
+	PCOM_VREG_LDO(ldo02, 13, NULL, 2100000, 2100000, 0, -1, 0, 1, 0, 0, p),
+	PCOM_VREG_LDO(ldo03, 49, NULL, 1200000, 1200000, 0, -1, 0, 1, 0, 0, n),
+	PCOM_VREG_LDO(ldo04, 50, NULL, 1100000, 1100000, 0, -1, 0, 1, 0, 0, n),
+	PCOM_VREG_LDO(ldo05, 45, NULL, 0, 0, 0, -1, 0, 0, 0, 0, n), /* not used */
+	PCOM_VREG_LDO(ldo06, 51, NULL, 1200000, 1200000, 0, -1, 0, 0, 0, 0, n),
+	PCOM_VREG_LDO(ldo07, 0, NULL, 2600000, 2600000, 0, -1, 0, 1, 0, 0, p),
+	PCOM_VREG_LDO(ldo08, 9, NULL, 2800000, 2800000, 0, -1, 0, 1, 0, 0, p),
+	PCOM_VREG_LDO(ldo09, 44, NULL, 1800000, 1800000, 0, -1, 0, 1, 0, 0, p),
+	PCOM_VREG_LDO(ldo10, 52, NULL, 2850000, 2850000, 0, -1, 0, 0, 0, 0, p),
+	PCOM_VREG_LDO(ldo11, 53, NULL, 1800000, 1800000, 0, -1, 0, 0, 0, 0, p),
+	PCOM_VREG_LDO(ldo12, 21, NULL, 3300000, 3300000, 0, -1, 0, 1, 0, 0, p),
+	PCOM_VREG_LDO(ldo13, 18, NULL, 2850000, 2850000, 0, -1, 0, 1, 0, 0, p),
+	PCOM_VREG_LDO(ldo14, 16, NULL, 3000000, 3300000, 0, -1, 0, 1, 0, 0, p),
+	PCOM_VREG_LDO(ldo15, 54, NULL, 1800000, 1800000, 0, -1, 0, 0, 0, 0, p),
+	PCOM_VREG_LDO(ldo16, 19, NULL, 3000000, 3000000, 0, -1, 0, 0, 0, 0, p),
+	PCOM_VREG_LDO(ldo17, 56, NULL, 2800000, 2800000, 0, -1, 0, 0, 0, 0, p),
+	PCOM_VREG_LDO(ldo18, 11, NULL, 2700000, 2700000, 0, -1, 0, 0, 0, 0, p),
+	PCOM_VREG_LDO(ldo19, 57, NULL, 3000000, 3000000, 0, -1, 0, 1, 0, 0, p),
+#else
 	PCOM_VREG_SMP(smps1,  3, NULL, 1100000, 1100000, 0, -1, 0, 0, 0, 0, s),
 	PCOM_VREG_SMP(smps2,  4, NULL, 1100000, 1100000, 0, -1, 0, 0, 0, 0, s),
 	PCOM_VREG_SMP(smps3,  2, NULL, 1800000, 1800000, 0, -1, 0, 0, 0, 0, s),
@@ -232,7 +279,7 @@ static struct proccomm_regulator_info msm7x27a_pcom_vreg_info[] = {
 	PCOM_VREG_LDO(ldo17, 56, NULL, 2900000, 3300000, 0, -1, 0, 0, 0, 0, p),
 	PCOM_VREG_LDO(ldo18, 11, NULL, 2700000, 2700000, 0, -1, 0, 0, 0, 0, p),
 	PCOM_VREG_LDO(ldo19, 57, NULL, 1200000, 1800000, 0, -1, 0, 0, 0, 0, p),
-
+#endif
 	PCOM_VREG_NCP(ncp,   31, NULL, -1800000, -1800000, 0,     0, 0, 0, 0),
 };
 
