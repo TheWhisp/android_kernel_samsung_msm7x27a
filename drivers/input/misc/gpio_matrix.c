794dde5c468ad7e381bfee30626b8e9f233d43d8
Erik Castricum <erikcas1972@gmail.com>
2013-11-05 09:54:27 +0100

    update lost code (1/2)

--- drivers/input/misc/gpio_matrix.c
+++ drivers/input/misc/gpio_matrix.c
@@ -35,6 +35,27 @@ struct gpio_kp {
 	unsigned long keys_pressed[0];
 };
 
+static struct gpio_kp *pgpio_key;
+
+extern struct class *sec_class;
+struct device *kpd_dev;
+unsigned int sec_key_pressed;
+static ssize_t keyshort_test(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	int count;
+	pr_info("[KEY] %s key %d\n",__func__, sec_key_pressed);
+
+	if (sec_key_pressed) {
+		count = sprintf(buf, "PRESS\n");
+	}
+	else {
+		count = sprintf(buf, "RELEASE\n");
+	}
+	return count;
+}
+
+static DEVICE_ATTR(sec_key_pressed, 0664, keyshort_test, NULL);
+
 static void clear_phantom_key(struct gpio_kp *kp, int out, int in)
 {
 	struct gpio_event_matrix_info *mi = kp->keypad_info;
@@ -112,24 +133,34 @@ static void report_key(struct gpio_kp *kp, int key_index, int out, int in)
 	unsigned short keycode = keyentry & MATRIX_KEY_MASK;
 	unsigned short dev = keyentry >> MATRIX_CODE_BITS;
 
+//	pr_info("[KEY TEST] %s, %d, key :%d, %d\n", __func__, __LINE__, keycode, pressed);
 	if (pressed != test_bit(keycode, kp->input_devs->dev[dev]->key)) {
 		if (keycode == KEY_RESERVED) {
-			if (mi->flags & GPIOKPF_PRINT_UNMAPPED_KEYS)
+			if (mi->flags & GPIOKPF_PRINT_UNMAPPED_KEYS){
 				pr_info("gpiomatrix: unmapped key, %d-%d "
 					"(%d-%d) changed to %d\n",
 					out, in, mi->output_gpios[out],
 					mi->input_gpios[in], pressed);
+				}
 		} else {
-			if (mi->flags & GPIOKPF_PRINT_MAPPED_KEYS)
+			if (mi->flags & GPIOKPF_PRINT_MAPPED_KEYS){
 				pr_info("gpiomatrix: key %x, %d-%d (%d-%d) "
 					"changed to %d\n", keycode,
 					out, in, mi->output_gpios[out],
 					mi->input_gpios[in], pressed);
+				}
+
+			//if (dump_enable_flag != 0)
+			///pr_info("[KEY] keycode: %d, %s\n", keycode, pressed ? "pressed" : "released");
+			printk("[KEY] keycode: %d, %s\n", keycode, pressed ? "pressed" : "released");
 			input_report_key(kp->input_devs->dev[dev], keycode, pressed);
+			sec_key_pressed = pressed;
+			
+			if (dump_enable_flag != 0)
+				sec_check_crash_key(keycode, pressed);
 		}
 	}
 }
-
 static void report_sync(struct gpio_kp *kp)
 {
 	int i;
@@ -352,7 +383,7 @@ int gpio_event_matrix_func(struct gpio_event_input_devs *input_devs,
 		}
 		key_count = mi->ninputs * mi->noutputs;
 
-		*data = kp = kzalloc(sizeof(*kp) + sizeof(kp->keys_pressed[0]) *
+		pgpio_key = *data = kp = kzalloc(sizeof(*kp) + sizeof(kp->keys_pressed[0]) *
 				     BITS_TO_LONGS(key_count), GFP_KERNEL);
 		if (kp == NULL) {
 			err = -ENOMEM;
@@ -423,6 +454,13 @@ int gpio_event_matrix_func(struct gpio_event_input_devs *input_devs,
 		wake_lock_init(&kp->wake_lock, WAKE_LOCK_SUSPEND, "gpio_kp");
 		err = gpio_keypad_request_irqs(kp);
 		kp->use_irq = err == 0;
+		
+				kpd_dev = device_create(sec_class, NULL, 0, NULL, "sec_key");
+		if (!kpd_dev)
+			printk(KERN_WARNING "Failed to create device(sec_key)!\n");
+		if (device_create_file(kpd_dev, &dev_attr_sec_key_pressed) < 0)
+			printk(KERN_WARNING "Failed to create file(%s)!\n"
+				, dev_attr_sec_key_pressed.attr.name);
 
 		pr_info("GPIO Matrix Keypad Driver: Start keypad matrix for "
 			"%s%s in %s mode\n", input_devs->dev[0]->name,
